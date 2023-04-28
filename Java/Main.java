e3157c14ecc3b62c8006671ac47efe994e8ec91e
Angelo94a <81625855+Angelo94a@users.noreply.github.com>
2023-04-28 09:33:17 -0500

    actualizado

--- Java/Main.java
+++ Java/Main.java
@@ -5,5 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        Car car = new Car();
+        car.license = "AMC123";
+        car.driver = "Angelo Almeida";
+        car.passegenger = 4;
+        car.printData();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
     print("hola mundo")
+    car = Car()
+    car.license = "AMS145"
+    car.driver = "Angelo Almeida"
+    print(vars(car))
+
+    car2 = Car()
+    car.license = "DTA1243"
+    car.driver = "vera almeida"
+    car.passegenger = 3
+    print(vars(car))
