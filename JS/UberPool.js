581b26749ca74dde7e24e50d5d289ab4f632f229
Angelo94a <81625855+Angelo94a@users.noreply.github.com>
2023-05-13 13:31:34 -0500

    agregando js al repositorio

---
+++ JS/UberPool.js
@@ -0,0 +1,7 @@
+class UberPool extends Car {
+	constructor(license, driver, brands, model) {
+		super(license, driver)
+		this.brands = brands
+		this.model = model
+	}
+}
---
+++ JS/UberX.js
@@ -0,0 +1,7 @@
+class UberX extends Car {
+	constructor(licesnse, driver, brand, model) {
+		super(licesnse, driver)
+		this.brand = brand
+		this.model = model
+	}
+}
